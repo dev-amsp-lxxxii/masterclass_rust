e263703efeb5748b76a20e0ba83fae436b6fff4e
Adelmo Menezes - Opensuse <ams.protazio@gmail.com>
2024-04-04 17:30:57 -0300

    Intro sobre as regras de ownership

---
+++ fundamentos/src/ex_stack_heap.rs
@@ -0,0 +1,19 @@
+fn ex_stack_heap() {
+    /*
+    fn main() {
+        // Heap
+        let nome: String = String::from("Rust");
+        // Stack
+        let idade: i32 = 32;
+
+        println!("Nome -> {}", nome);
+        // println!("Idade -> {}", idade);
+        exibir_idade(&idade);
+    }
+
+    // Stack
+    fn exibir_idade(idade: &i32) {
+        println!("Idade -> {}", idade);
+    }
+    */
+}
--- fundamentos/src/main.rs
+++ fundamentos/src/main.rs
@@ -1,15 +1,13 @@
 fn main() {
-    // Heap
+    // Regras de Ownership
+    // Quando se trabalha na heap o valor da variável pertencerá apenas a uma variável
+
     let nome: String = String::from("Rust");
-    // Stack
-    let idade: i32 = 32;
+    // Transferência de posse do valor da variável #nome para #new_nome
+    let new_nome: String = nome;
 
-    println!("Nome -> {}", nome);
-    // println!("Idade -> {}", idade);
-    exibir_idade(&idade);
-}
+    // println!("Nome -> {}", nome); -> da erro, pois o valor não pertence mais a variável nome.
+    println!("Nome -> {}", new_nome);
 
-// Stack
-fn exibir_idade(idade: &i32) {
-    println!("Idade -> {}", idade);
+    // A posse verticalmente e sequêncial
 }
