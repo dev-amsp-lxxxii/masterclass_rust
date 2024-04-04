c68a428449b77b08407aa026f2166a41c9efd063
Adelmo Menezes - Opensuse <ams.protazio@gmail.com>
2024-04-03 23:29:26 -0300

    Criado o projeto fundamentos

---
+++ fundamentos/target/debug/deps/fundamentos-68ed16ef9eea0c44.d
@@ -0,0 +1,5 @@
+/home/adelmo/dev_estudo/masterclass_rust/fundamentos/target/debug/deps/fundamentos-68ed16ef9eea0c44: src/main.rs
+
+/home/adelmo/dev_estudo/masterclass_rust/fundamentos/target/debug/deps/fundamentos-68ed16ef9eea0c44.d: src/main.rs
+
+src/main.rs:
---
+++ fundamentos/target/debug/fundamentos.d
@@ -0,0 +1,1 @@
+/home/adelmo/dev_estudo/masterclass_rust/fundamentos/target/debug/fundamentos: /home/adelmo/dev_estudo/masterclass_rust/fundamentos/src/main.rs
