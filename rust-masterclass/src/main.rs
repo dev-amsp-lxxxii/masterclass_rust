298eba3e03aa1c89232012ac6ea340654080670d
Adelmo Menezes - Opensuse <ams.protazio@gmail.com>
2024-04-04 19:11:58 -0300

    Cocluido o menu do masterclass

--- rust-masterclass/src/main.rs
+++ rust-masterclass/src/main.rs
@@ -1,20 +1,24 @@
 mod utils;
 
-use utils::terminal::{esperar_enter, exibir_menu};
+use std::process::exit;
+
+use utils::terminal::{esperar_enter, exibir_menu, limpar_tela};
 
 fn main() {
-    let itens = ["Fundamentos", "Tipos", "Controle", "Funções", "Ownership"];
+    loop {
+        let itens = ["Fundamentos", "Tipos", "Controle", "Funções", "Ownership"];
 
-    let selecionado = exibir_menu("Principal", &itens, true);
-    esperar_enter();
-    /*
-    println!("Hello, world!");
-    println!("Hello, world!");
-    println!("Hello, world!");
-    println!("Hello, world!");
-    println!("Hello, world!");
-    esperar_enter();
-    limpar_tela();
-    println!("Hello, world!");
-    */
+        let selecionado = exibir_menu("Principal", &itens, true);
+
+        limpar_tela();
+        match selecionado {
+            1 => println!("1"),
+            2 => println!("2"),
+            3 => println!("3"),
+            4 => println!("4"),
+            5 => println!("5"),
+            _ => exit(0),
+        }
+        esperar_enter();
+    }
 }
