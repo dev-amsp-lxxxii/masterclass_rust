1b9b617a4ec37b14eac2ac10350ca3437bdf82b8
Adelmo Menezes - Opensuse <ams.protazio@gmail.com>
2024-04-05 14:19:19 -0300

    Cocluido os tipos

--- rust-masterclass/src/tipos/custom.rs
+++ rust-masterclass/src/tipos/custom.rs
@@ -1,5 +1,54 @@
+use std::fmt;
 
+pub fn exemplo_struct() {
+    struct Usuario {
+        nome: String,
+        email: String,
+        ativo: bool,
+        idade: u8,
+    }
 
-pub fn exemplo_struct() {}
+    let usuario = Usuario {
+        nome: String::from("João"),
+        email: String::from("jjj@abcd.com.br"),
+        ativo: true,
+        idade: 25,
+    };
 
-pub fn exemplo_enum() {}
+    println!("usuario => {} {}", usuario.nome, usuario.email);
+    println!("usuario => {} {}", usuario.ativo, usuario.idade);
+}
+
+pub fn exemplo_enum() {
+    enum DiasDaSemana {
+        Segunda,
+        Terca,
+        Quarta,
+        Quinta,
+        Sexta,
+        Sabado,
+        Domingo,
+    }
+
+    impl fmt::Display for DiasDaSemana {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            match self {
+                DiasDaSemana::Segunda => write!(f, "Segunda"),
+                DiasDaSemana::Terca => write!(f, "Terça"),
+                DiasDaSemana::Quarta => write!(f, "Quarta"),
+                DiasDaSemana::Quinta => write!(f, "Quinta"),
+                DiasDaSemana::Sexta => write!(f, "Sexta"),
+                DiasDaSemana::Sabado => write!(f, "Sábado"),
+                DiasDaSemana::Domingo => write!(f, "Domingo"),
+            }
+        }
+    }
+
+    println!("dia => {}", DiasDaSemana::Domingo);
+    println!("dia => {}", DiasDaSemana::Segunda);
+    println!("dia => {}", DiasDaSemana::Terca);
+    println!("dia => {}", DiasDaSemana::Quarta);
+    println!("dia => {}", DiasDaSemana::Quinta);
+    println!("dia => {}", DiasDaSemana::Sexta);
+    println!("dia => {}", DiasDaSemana::Sabado);
+}
